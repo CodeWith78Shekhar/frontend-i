bf4be949d58b24bbc88d50eeb73924329b6492f3
coder shekhar <shekharsharma0119@gmail.com>
2023-12-26 14:15:13 +0530

    Dom practice with css

---
+++ javascript/index1.js
@@ -0,0 +1,101 @@
+//  console.log("hello world")
+
+// var i = 5;
+// // console.log(i)
+
+// {
+//     var i = 10;
+//     i = 20;
+//     console.log(i)
+// }
+
+//  const i= 5;
+// console.log(i)
+
+// {
+//     const i = 10;
+//      i = 20;
+//     console.log(i)
+// }
+
+
+// let a = 80;
+// let b = 120;
+// let c = 70;
+// let d = 130;
+
+// if(a>b){
+//     if(a>c){
+//         if(a>d){
+//             console.log(a);
+//         }
+//     }
+// }
+// else if(b>a){
+//     if(b>c){
+//         if(b>d){
+//             console.log(b);
+//         }
+//     }
+// }
+//  else if(c>d){
+//     if(c>a){
+//         if(c>b){
+//             console.log(c);
+//         }
+//     }
+// }else{
+//     console.log(d);
+// }
+
+// temary operator
+
+
+//  let a = 11;
+//   a%2 ==0 ? console.log("even") : console.log("odd");
+
+//  if(result==0){
+//     console.log("it is even")
+//  }else{
+//     console.log(" its a odd number")
+//  }
+
+//Switch operator
+  
+let a = 3;
+
+
+  switch (a) {
+    case 1:
+      console.log("monday"); 
+    break;
+
+    case 2:
+      console.log("tuesday"); 
+    break;
+
+    case 3:
+      console.log("wednesday"); 
+    break;
+
+    case 4:
+      console.log("thrusday"); 
+    break;
+
+    case 5:
+      console.log("friday"); 
+    break;
+
+    case 6:
+      console.log("saturday"); 
+    break;
+
+    case 7:
+      console.log("sunday"); 
+    break;
+
+    default:
+        console.log("wrong no")
+        break;
+  }
+
